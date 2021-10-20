8f94edf6d8c4d1dcc5541f34179f5e08a1d116f2
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:10:05 +0700

    Add files via upload

---
+++ Task12/Lab12/Lab12/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(466, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Football Championship 2021";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(526, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 256);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "VS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(561, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Round1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(737, 251);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(123, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(956, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "VS";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1065, 251);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(206, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Winner";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(171, 324);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(123, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(918, 324);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(123, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(947, 294);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Winner";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(561, 360);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Round2";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(315, 399);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(132, 22);
+            this.textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(728, 399);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(132, 22);
+            this.textBox8.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(571, 404);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 17);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "VS";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(498, 441);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(171, 17);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Winner of Championship!!!";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(515, 477);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(132, 22);
+            this.textBox9.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(408, 58);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(381, 17);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Enter the Average Goals scored in the match by each team";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(111, 99);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(80, 22);
+            this.textBox10.TabIndex = 27;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(352, 99);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(80, 22);
+            this.textBox11.TabIndex = 28;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(861, 94);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(80, 22);
+            this.textBox12.TabIndex = 29;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(1130, 91);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(80, 22);
+            this.textBox13.TabIndex = 30;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 102);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 17);
+            this.label11.TabIndex = 31;
+            this.label11.Text = "Barcelona:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(279, 104);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 17);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "Texas:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(725, 97);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(113, 17);
+            this.label13.TabIndex = 33;
+            this.label13.Text = "Manchester City:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(1039, 94);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(70, 17);
+            this.label14.TabIndex = 34;
+            this.label14.Text = "Liverpool:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(1195, 11);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(28, 22);
+            this.textBox14.TabIndex = 35;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(1146, 14);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(43, 17);
+            this.label15.TabIndex = 36;
+            this.label15.Text = "Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 511);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Task12/Lab12/Lab12/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double ProbabilityofGoalsbyBarcelona;
+        double ProbabilityofGoalsbyTexas;
+        double ProbabilityofGoalsbyManchester;
+        double ProbabilityofGoalsbyLiverpool;
+
+        int k1 = 5; //Expected number of goals by Barcelona
+        int k2 = 5; //Expected number of goals by Texas
+        int k3 = 3; //Expected number of goals by Manchester City
+        int k4 = 3; //Expected number of goals by Liverpool
+
+
+        int rollingtime = 0;
+
+        public double factorial_Recursion(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rollingtime++;
+
+            textBox14.Text = rollingtime.ToString();
+            int newroll = 0;
+
+            double lambda1 = Convert.ToDouble(textBox10.Text);  //Average Goals scored in the match by Barcelona
+            double lambda2 = Convert.ToDouble(textBox11.Text); //Average Goals scored in the match by Texas
+            double lambda3 = Convert.ToDouble(textBox12.Text);  //Average Goals scored in the match by Manchester City
+            double lambda4 = Convert.ToDouble(textBox13.Text);  //Average Goals scored in the match by Liverpool
+
+            textBox1.Text = "Barcelona";
+            textBox2.Text = "Texas";
+            textBox3.Text = "Manchester City";
+            textBox4.Text = "Liverpool";
+
+
+            //Calculating Probability of scoring 5 goals by Barcelona Team
+            //Hence, calculating P(5) by Poisson Distribution which is given by P(k) = (lambda^k e^-lambda)/k! where k = 5;
+
+            double numerator1 = Math.Pow(lambda1, k1);
+            double denominator1 = factorial_Recursion(k1);
+            double MultiplierNum1 = Math.Exp(-lambda1);
+
+            ProbabilityofGoalsbyBarcelona = (numerator1 / denominator1) * MultiplierNum1;
+
+            //Calculating Probability of scoring 5 goals by Texas Team
+            //Hence, calculating P(5) by Poisson Distribution which is given by P(k) = (lambda^k e^-lambda)/k! where k = 5;
+
+            double numerator2 = Math.Pow(lambda2, k2);
+            double denominator2 = factorial_Recursion(k2);
+            double MultiplierNum2 = Math.Exp(-lambda2);
+
+            ProbabilityofGoalsbyTexas = (numerator2 / denominator2) * MultiplierNum2;
+
+
+            //Calculating Probability of scoring 3 goals by Manchester City Team
+            //Hence, calculating P(3) by Poisson Distribution which is given by P(k) = (lambda^k e^-lambda)/k! where k = 3;
+
+            double numerator3 = Math.Pow(lambda3, k3);
+            double denominator3 = factorial_Recursion(k3);
+            double MultiplierNum3 = Math.Exp(-lambda3);
+
+            ProbabilityofGoalsbyManchester = (numerator3 / denominator3) * MultiplierNum3;
+
+            //Calculating Probability of scoring 3 goals by Liverpool Team
+            //Hence, calculating P(5) by Poisson Distribution which is given by P(k) = (lambda^k e^-lambda)/k! where k = 3;
+
+            double numerator4 = Math.Pow(lambda4, k4);
+            double denominator4 = factorial_Recursion(k4);
+            double MultiplierNum4 = Math.Exp(-lambda4);
+
+            ProbabilityofGoalsbyLiverpool = (numerator4 / denominator4) * MultiplierNum4;
+
+
+            if (textBox14.Text == "40")
+            {
+                //timer1.Stop();
+                //rollingtime = newroll;
+
+                if (ProbabilityofGoalsbyBarcelona > ProbabilityofGoalsbyTexas)
+                {
+                    textBox5.Text = "Barcelona";
+                    textBox7.Text = "Barcelona";
+
+
+                }
+                else
+                {
+                    textBox5.Text = "Texas";
+                    textBox7.Text = "Texas";
+
+                }
+
+                if (ProbabilityofGoalsbyManchester > ProbabilityofGoalsbyLiverpool)
+                {
+                    textBox6.Text = "Manchester";
+                    textBox8.Text = "Manchester";
+
+                }
+
+                else
+                {
+                    textBox6.Text = "Liverpool";
+                    textBox8.Text = "Liverpool";
+
+                }
+
+            }
+
+            if (textBox14.Text == "70")
+            {
+                timer1.Stop();
+                rollingtime = newroll;
+                if (textBox7.Text == "Barcelona" && textBox8.Text == "Manchester")
+                {
+                    if (ProbabilityofGoalsbyBarcelona > ProbabilityofGoalsbyManchester)
+                    {
+                        textBox9.Text = "Barcelona";
+                    }
+                    else
+                    {
+                        textBox9.Text = "Manchester";
+
+                    }
+
+                }
+
+                if (textBox7.Text == "Barcelona" && textBox8.Text == "Liverpool")
+                {
+                    if (ProbabilityofGoalsbyBarcelona > ProbabilityofGoalsbyLiverpool)
+                    {
+                        textBox9.Text = "Barcelona";
+                    }
+                    else
+                    {
+                        textBox9.Text = "Liverpool";
+
+                    }
+
+                }
+
+
+                if (textBox7.Text == "Texas" && textBox8.Text == "Manchester")
+                {
+                    if (ProbabilityofGoalsbyTexas > ProbabilityofGoalsbyManchester)
+                    {
+                        textBox9.Text = "Texas";
+                    }
+                    else
+                    {
+                        textBox9.Text = "Manchester";
+
+                    }
+
+                }
+
+                if (textBox7.Text == "Texas" && textBox8.Text == "Liverpool")
+                {
+                    if (ProbabilityofGoalsbyTexas > ProbabilityofGoalsbyLiverpool)
+                    {
+                        textBox9.Text = "Texas";
+                    }
+                    else
+                    {
+                        textBox9.Text = "Liverpool";
+
+                    }
+
+                }
+
+            }
+            
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
