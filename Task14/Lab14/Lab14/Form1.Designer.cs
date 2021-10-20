d5d23e0427b6c3f251e62bc39b956b5bb1543b94
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:16:50 +0700

    Add files via upload

---
+++ Task14/Lab14/Lab14/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mean";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Variance";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(98, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(98, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of Experiments";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(209, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(98, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(801, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Emperical Probabilities";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "Theoretical Probabilities";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(871, 506);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(681, 168);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(99, 17);
+            this.label13.TabIndex = 71;
+            this.label13.Text = "> 11.07 is true";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(584, 165);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(91, 22);
+            this.textBox8.TabIndex = 70;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(415, 165);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(163, 17);
+            this.label12.TabIndex = 69;
+            this.label12.Text = "Chi-Squared Test Value:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(653, 95);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(91, 22);
+            this.textBox7.TabIndex = 68;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(603, 95);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 17);
+            this.label11.TabIndex = 67;
+            this.label11.Text = "Error:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(653, 32);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(91, 22);
+            this.textBox5.TabIndex = 66;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(603, 37);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 17);
+            this.label10.TabIndex = 65;
+            this.label10.Text = "Error:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(489, 95);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(91, 22);
+            this.textBox6.TabIndex = 64;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(415, 95);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 63;
+            this.label9.Text = "Variance:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(489, 34);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(91, 22);
+            this.textBox4.TabIndex = 62;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(418, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 17);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "Average:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1763, 554);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Task14/Lab14/Lab14/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab14
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        double[] Statistics1 = new double[500];
+        double[] Statistics2 = new double[500];
+
+        double[] RelativeFrequencies = new double[5]; //initializing an empty array
+
+        double Alpha1;
+        double Alpha2;
+
+        double RV1;
+        double RV2;
+
+        Random random = new Random();
+        public double RandomDoubleInclusive() //We are using this method because random.NextDouble() method gives random number 
+                                              //between 0 and 1 where 0 is inclusive and 1 is exclusive. 
+                                              //Since the value of probability lies between 0 and 1, both inclusive that's why we need 
+                                              //to use this method.
+        {
+            double d = 0.0;
+            int i = 0;
+
+            do
+            {
+                d = random.NextDouble();
+                i = random.Next(2);
+            }
+            while (i == 1 && d > 0);
+
+            return d + i;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int i,j;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SampleSize = Convert.ToInt32(textBox3.Text);
+            
+
+            for ( i = 0; i<500;)
+            {
+                for (j = 0; j < 500;)
+                {
+                    Alpha1 = RandomDoubleInclusive();
+                    Alpha2 = RandomDoubleInclusive();
+
+                    double LnPart = Math.Log(Alpha1);
+
+                    double part1 = (-2) * LnPart;
+
+                    double part2 = 2 * 3.14159 * Alpha2;
+
+                    double CosPart = Math.Cos(part2);
+
+                    double SinPart = Math.Sin(part2);
+
+
+                    RV1 = Math.Sqrt(part1) * CosPart;
+                    Statistics1[i] = RV1;
+
+                    RV2 = Math.Sqrt(part1) * SinPart;
+                    Statistics2[j] = RV2;
+
+
+                    i++;
+                    j++;
+                }
+
+            }
+
+            var myList = new List<double>();
+            myList.AddRange(Statistics1);
+            myList.AddRange(Statistics2);
+            double[] arr3 = myList.ToArray();
+
+            
+            double Max = arr3.Max();
+
+            double Min = arr3.Min();
+            
+
+            int ClosestIntMin = (int )Math.Round(Min);
+          
+            int ClosestIntMax = (int)Math.Round(Max);
+            
+
+            double differenceofMaxMin = Max - Min;
+
+            double k;
+
+            k = Math.Log(SampleSize,2) + 1;
+
+
+
+            int[,] Intervals = new int[,]{ { -4, -2 }, { -2, 0 },
+              { 0, 2 }, { 2, 4 } };
+
+            int LengthofarrayIntervals = Intervals.GetLength(0);
+
+            int Lengthofarr3 = arr3.GetLength(0);
+            // Variable to store the count of intervals
+            int frequencyininterval1 = 0;
+            int frequencyininterval2 = 0;
+            int frequencyininterval3 = 0;
+            int frequencyininterval4 = 0;
+            // Variables to store start and end of an interval
+            int li, ri;
+
+            int V;
+            for (V = 0; V < Lengthofarr3;)
+            {
+                for (int i = 0; i <1; i++)
+                {
+                    li = Intervals[i, 0];
+                    ri = Intervals[i, 1];
+
+                    // Implies V lies in the interval
+                    // so increase count
+                    if (arr3[V] >= li && arr3[V] <= ri)
+                        frequencyininterval1++;
+                        
+
+                }
+
+                for (int i = 1; i < 2; i++)
+                {
+                    li = Intervals[i, 0];
+                    ri = Intervals[i, 1];
+
+                    // Implies V lies in the interval
+                    // so increase count
+                    if (arr3[V] >= li && arr3[V] <= ri)
+                        frequencyininterval2++;
+                    
+                }
+
+                for (int i = 2; i < 3; i++)
+                {
+                    li = Intervals[i, 0];
+                    ri = Intervals[i, 1];
+
+                    // Implies V lies in the interval
+                    // so increase count
+                    if (arr3[V] >= li && arr3[V] <= ri)
+                        frequencyininterval3++;
+                   
+                }
+
+                for (int i = 3; i < 4; i++)
+                {
+                    li = Intervals[i, 0];
+                    ri = Intervals[i, 1];
+
+                    // Implies V lies in the interval
+                    // so increase count
+                    if (arr3[V] >= li && arr3[V] <= ri)
+                        frequencyininterval4++;
+                   
+                }
+                V++;
+            }
+            double relativefrequency1;
+            relativefrequency1 = (double)frequencyininterval1 / SampleSize;
+            chart1.Series["Emperical Probabilities"].Points.AddXY(0, relativefrequency1);
+           
+
+            double relativefrequency2 = (double)frequencyininterval2 / 1000;
+            chart1.Series["Emperical Probabilities"].Points.AddXY(0, relativefrequency2);
+
+            double relativefrequency3 = (double)frequencyininterval3 / 1000;
+            chart1.Series["Emperical Probabilities"].Points.AddXY(0, relativefrequency3);
+
+            double relativefrequency4 = (double)frequencyininterval4 / 1000;
+            chart1.Series["Emperical Probabilities"].Points.AddXY(0, relativefrequency4);
+
+            chart1.Series[0].Points[0].AxisLabel = "(-4;-2]";
+            chart1.Series[0].Points[1].AxisLabel = "(-2;0]";
+            chart1.Series[0].Points[2].AxisLabel = "(0;2]";
+            chart1.Series[0].Points[3].AxisLabel = "(2;4]";
+
+            double p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
+
+
+            double denominator = Math.Sqrt(2 * 3.141);
+
+
+            double power1 = (double)-1 / 2;
+            double power2 = (double)-4 / 2;
+            double power3 = (double)-9 / 2;
+            double power4 = (double)-16 / 2;
+            double power5 = (double)-25 / 2;
+            double power6 = (double)-1 / 2;
+            double power7 = (double)-4 / 2;
+            double power8 = (double)-9 / 2;
+            double power9 = (double)-16 / 2;
+            double power10 = (double)-25 / 2;
+
+            double numerator1 = Math.Pow(2.71828, power1);
+            double numerator2 = Math.Pow(2.71828, power2);
+            double numerator3 = Math.Pow(2.71828, power3);
+            double numerator4 = Math.Pow(2.71828, power4);
+            double numerator5 = Math.Pow(2.71828, power5);
+            double numerator6 = Math.Pow(2.71828, power6);
+            double numerator7 = Math.Pow(2.71828, power7);
+            double numerator8 = Math.Pow(2.71828, power8);
+            double numerator9 = Math.Pow(2.71828, power9);
+            double numerator10 = Math.Pow(2.71828, power10);
+
+            p1 = numerator1 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p1);
+
+            p2 = numerator2 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p2);
+            p3 = numerator3 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p3);
+            p4 = numerator4 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p4);
+            p5 = numerator5 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p5);
+
+            p6 = numerator6 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p6);
+
+            p7 = numerator7 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p7);
+            p8 = numerator8 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p8);
+            p9 = numerator9 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p9);
+            p10 = numerator10 / denominator;
+            chart1.Series["Theoretical Probabilities"].Points.AddXY(0, p10);
+
+            double EmpericalAverage;
+            int m;
+            int sum = 0;
+
+            for (m = 1; m <= SampleSize; m++)
+            {
+                sum = sum + m;
+               
+            }
+
+            EmpericalAverage = sum / SampleSize;
+            textBox4.Text = EmpericalAverage.ToString();
+
+            double squareofaverage = sum * sum;
+
+            double EmpericalVariance;
+            int n;
+            int summ = 0;
+            int squareofn;
+
+            for (n = 1 ; n <= SampleSize; n++)
+            {
+                squareofn = n * n;
+                summ = summ + squareofn;
+
+            }
+
+            double squareofempericalAverage = EmpericalAverage * EmpericalAverage;
+
+            double numerator = summ - squareofempericalAverage;
+
+            EmpericalVariance = numerator / SampleSize;
+            textBox6.Text = EmpericalVariance.ToString();
+
+            double Mean = Convert.ToInt32(textBox1.Text);
+            double Variance = Convert.ToInt32(textBox2.Text);
+
+            double AbsoluteErrorinMean = EmpericalAverage - Mean;
+
+            double AbsoluteErrorinVariance = EmpericalVariance - Variance;
+
+            double RelativeErrorinMean = AbsoluteErrorinMean / Mean;
+
+            textBox5.Text = Math.Abs(RelativeErrorinMean).ToString();
+
+            double RelativeErrorinVariance = AbsoluteErrorinVariance / Variance;
+
+            textBox7.Text = Math.Abs(RelativeErrorinVariance).ToString();
+
+            double ChisquaredValue;
+
+            double squareofni1 = frequencyininterval1 * frequencyininterval1;
+            double squareofni2 = frequencyininterval2 * frequencyininterval2;
+            double squareofni3 = frequencyininterval3 * frequencyininterval3;
+            double squareofni4 = frequencyininterval4 * frequencyininterval4;
+
+            
+            double denominator1 = SampleSize * p1;
+            double denominator2 = SampleSize * p2;
+            double denominator3 = SampleSize * p3;
+            double denominator4 = SampleSize * p4;
+
+            double calculatenumeratordenominator1 = squareofni1 / denominator1;
+            double calculatenumeratordenominator2 = squareofni2 / denominator2;
+            double calculatenumeratordenominator3 = squareofni3 / denominator3;
+            double calculatenumeratordenominator4 = squareofni4 / denominator4;
+
+            ChisquaredValue = (calculatenumeratordenominator1 - SampleSize) + (calculatenumeratordenominator2 - SampleSize) + (calculatenumeratordenominator3 - SampleSize) + (calculatenumeratordenominator4 - SampleSize);
+            textBox8.Text = ChisquaredValue.ToString();
+        }
+    }
+}
