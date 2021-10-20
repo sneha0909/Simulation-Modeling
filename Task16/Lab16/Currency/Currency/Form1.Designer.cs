33d30a162c9f1afc03bc7ad55c50ec919bbd01cd
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:22:19 +0700

    Add files via upload

---
+++ Task16/Lab16/Currency/Currency/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Currency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.edDeviation = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edDrift = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edDays = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edPrice = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.edDeviation);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.edDrift);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.edDays);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edPrice);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(972, 57);
+            this.panel1.TabIndex = 0;
+            // 
+            // edDeviation
+            // 
+            this.edDeviation.Location = new System.Drawing.Point(744, 18);
+            this.edDeviation.Name = "edDeviation";
+            this.edDeviation.Size = new System.Drawing.Size(44, 22);
+            this.edDeviation.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(607, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Standard deviation:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Drift:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // edDrift
+            // 
+            this.edDrift.Location = new System.Drawing.Point(547, 17);
+            this.edDrift.Name = "edDrift";
+            this.edDrift.Size = new System.Drawing.Size(44, 22);
+            this.edDrift.TabIndex = 5;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(859, 8);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(100, 28);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Start!";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edDays
+            // 
+            this.edDays.Location = new System.Drawing.Point(349, 18);
+            this.edDays.Margin = new System.Windows.Forms.Padding(4);
+            this.edDays.Name = "edDays";
+            this.edDays.Size = new System.Drawing.Size(137, 22);
+            this.edDays.TabIndex = 3;
+            this.edDays.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of days:";
+            // 
+            // edPrice
+            // 
+            this.edPrice.DecimalPlaces = 2;
+            this.edPrice.Location = new System.Drawing.Point(82, 14);
+            this.edPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.edPrice.Name = "edPrice";
+            this.edPrice.Size = new System.Drawing.Size(139, 22);
+            this.edPrice.TabIndex = 1;
+            this.edPrice.Value = new decimal(new int[] {
+            7462,
+            0,
+            0,
+            131072});
+            this.edPrice.ValueChanged += new System.EventHandler(this.edPrice_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Initial price:";
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 57);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsValueShownAsLabel = true;
+            series2.LabelFormat = "f2";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(972, 497);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 554);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox edDrift;
+        private System.Windows.Forms.TextBox edDeviation;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Task16/Lab16/Currency/Currency/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            //Random random = new Random();
+            double price = (double)edPrice.Value;
+            int days = (int)edDays.Value;
+            double drift = Convert.ToDouble(edDrift.Text);
+            double standarddeviation = Convert.ToDouble(edDeviation.Text);
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, price);
+
+
+            double squareofdeviation = standarddeviation * standarddeviation;
+            double divisionpart = squareofdeviation / 2;
+
+            double part1 = drift - divisionpart;
+
+            int part2 = (days + 1) - days;
+
+            double ti = 0.5;
+
+            
+
+            double[] Statistics = new double[10000];
+
+
+            //for (int i = 1; i <= days; i++)
+            //{
+
+            //    double randomtimes = new Random().NextDouble();
+
+            //    Statistics[i] = randomtimes;
+            //    i++;
+
+  
+            //}
+
+          
+
+            for (int day = 1; day <= days; day++)
+            {
+                //double random = new Random().NextDouble();   //N(0,1)
+                //double squareroo = Statistics[day + 1] - Statistics[day]; //(ti + 1) - ti
+
+                //ti = Statistics[day] + Math.Sqrt(squareroo) * random; // w(ti + 1) = w(ti) + sqrt(ti+1 - ti)*N(0,1) : Simulation formula for Brownian Motion(Weiner Process)
+
+                double part3 = standarddeviation * ti;
+
+                double exponentialpart = part1 * part2 + part3;
+
+                //price = price * (1 + k * (random.NextDouble() - 0.5));
+
+                price = price * Math.Exp(exponentialpart);
+                chart1.Series[0].Points.AddXY(day, price);
+                
+            }
+        }
+
+        private void edPrice_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
