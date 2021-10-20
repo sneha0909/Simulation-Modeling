53292d6da4c9fe6e699b62efd655369ed3af7d68
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:20:27 +0700

    Add files via upload

---
+++ Task15/Lab15/Lab15/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(824, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 26);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(577, 314);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(850, 314);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(78, 22);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1091, 314);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(78, 22);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(577, 260);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(78, 22);
+            this.textBox6.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(850, 260);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(78, 22);
+            this.textBox7.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1091, 260);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(78, 22);
+            this.textBox8.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Time instant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(838, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Weather";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(509, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 178);
+            this.pictureBox1.TabIndex = 30;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(781, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 178);
+            this.pictureBox2.TabIndex = 31;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1029, 67);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(208, 178);
+            this.pictureBox3.TabIndex = 32;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 514);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Task15/Lab15/Lab15/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        double[] Arrayoftime = new double[] { 0, 0, 0, 0, 0 };   //Array of sequence of time 
+        int[] Arrayofstates = new int[] { 0, 0, 0, 0, 0 }; //Array of sequence of states 
+
+        double[] ArrayofProbabilities = new double[] { 0,0,0,0,0};
+
+
+        double[,] Q = new double[,]{{ -0.4, 0.3, 0.1},
+                            {0.4, -0.8, 0.4},
+                            {0.1, 0.4, -0.5}};
+
+
+
+        int i = 0;
+        int j = 0;
+        double t = 0;
+        int RV;
+        int k;
+        double p1;
+        double p2;
+        double p3;
+
+        //int[] Statistics = new int[10000];
+        //double[] Frequency = new double[100000]; //initializing an empty array
+
+        //double t;
+
+        //int NumberofExperiments = 10000; //N number of Simulation Experiments
+
+        Random random = new Random();
+        public double RandomDouble() //We are using this method because random.NextDouble() method gives random number 
+                                     //between 0 and 1 
+        {
+
+            double rnd = random.NextDouble();
+
+            return rnd;
+
+        }
+
+        
+
+
+        public double RandomDoubleInclusive() //We are using this method because random.NextDouble() method gives random number 
+                                              //between 0 and 1 where 0 is inclusive and 1 is exclusive. 
+                                              //Since the value of probability lies between 0 and 1, both inclusive that's why we need 
+                                              //to use this method.
+        {
+            double d = 0.0;
+            int i = 0;
+
+            do
+            {
+                d = random.NextDouble();
+                i = random.Next(2);
+            }
+            while (i == 1 && d > 0);
+
+
+
+            return d + i;
+
+
+
+        }
+
+        private void GenerateRV()
+        {
+           
+            double A = RandomDoubleInclusive();
+            for (k = 0; k < 5;)
+            {
+                for (int b = 0; b < ArrayofProbabilities.Length; b++) //Looping over array of probabilities
+                {
+
+                    A = A - p1;
+
+                    if (A <= 0)
+                    {
+
+                        //Arrayofstates[b] = k;
+                        switch (k) // 9 Different cases in Cheating dice 
+                        {
+
+                            case 0:
+
+                                pictureBox1.Image = Properties.Resources.sunny;
+
+                                textBox6.Text = "Sunny";
+                                textBox1.Text = Arrayoftime[0].ToString();
+                            
+                               
+
+                                break;
+                            case 1:
+                                pictureBox2.Image = Properties.Resources.snowy;
+                                textBox7.Text = "Snow";
+                                textBox2.Text = Arrayoftime[1].ToString();
+
+                                break;
+                            case 2:
+
+                                pictureBox3.Image = Properties.Resources.download;
+                                textBox8.Text = "Rainy";
+                                textBox3.Text = Arrayoftime[2].ToString();
+                                break;
+                        }
+                                break;
+
+
+                    }
+
+
+                    else
+                    {
+                        k++;
+
+                    }
+                }
+                break;
+
+
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double alpha = RandomDouble();
+
+           
+
+                for (int i = 0; i < 3;)
+                {
+                    double logValue = Math.Log(alpha);
+
+                    double tau = logValue / Q[i, i];
+
+                    t = t + tau;
+
+                    //Arrayoftime[i] = t;
+                    Arrayoftime.SetValue(t, i);
+
+                    Q[i, j] = -Q[i, j] / Q[i, i];
+
+                    p1 = Q[i, j];
+
+
+                    Q[i, j + 1] = -Q[i, j + 1] / Q[i, i];
+
+                    p2 = Q[i, j + 1];
+
+                    textBox1.Text = Q[i, j + 1].ToString();
+
+                    Q[i, j + 2] = -Q[i, j + 2] / Q[i, i];
+
+                    p3 = Q[i, j + 2];
+
+
+
+
+                    ArrayofProbabilities[0] = p1;
+                    ArrayofProbabilities[1] = p2;
+                    ArrayofProbabilities[2] = p3;
+
+
+                    i++;
+                    GenerateRV();
+                }
+            
+
+       
+
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+
+}
+
+
