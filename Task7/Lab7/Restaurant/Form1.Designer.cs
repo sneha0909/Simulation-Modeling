5f1b7fb2a4d55d90cf3ad70dd77aeaeab30de99d
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 19:49:00 +0700

    Add files via upload

---
+++ Task7/Lab7/Restaurant/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Restaurant
+{
+    partial class Restaurant
+    {
+        // <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartSample = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCus = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSample)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartSample
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.Title = "/Per Day";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chartSample.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            legend1.Title = "Variables:";
+            legend1.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.chartSample.Legends.Add(legend1);
+            this.chartSample.Location = new System.Drawing.Point(12, 60);
+            this.chartSample.Name = "chartSample";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Customers";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Resources";
+            this.chartSample.Series.Add(series1);
+            this.chartSample.Series.Add(series2);
+            this.chartSample.Size = new System.Drawing.Size(645, 381);
+            this.chartSample.TabIndex = 0;
+            this.chartSample.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resources in Restaurant:";
+            // 
+            // txtCus
+            // 
+            this.txtCus.Location = new System.Drawing.Point(188, 8);
+            this.txtCus.Name = "txtCus";
+            this.txtCus.Size = new System.Drawing.Size(100, 20);
+            this.txtCus.TabIndex = 2;
+            this.txtCus.TextChanged += new System.EventHandler(this.txtCus_TextChanged);
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(188, 34);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.ReadOnly = true;
+            this.txtRes.Size = new System.Drawing.Size(100, 20);
+            this.txtRes.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Available Customers:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(341, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(422, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Restaurant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 456);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chartSample);
+            this.Name = "Restaurant";
+            this.Text = "Restaurant";
+            ((System.ComponentModel.ISupportInitialize)(this.chartSample)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSample;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCus;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Task7/Lab7/Restaurant/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    /// <summary>
+    /// Assumption:
+    /// More Available Customers -> Resources decreases
+    /// Assume atleast 1 customer consumes 1/2 of our food resources
+    /// </summary>
+    public partial class Restaurant : Form
+    {
+        public Restaurant()
+        {
+            InitializeComponent();
+            chartSample.Series["Customers"].XValueMember = "X"; //Customers
+            chartSample.Series["Resources"].YValueMembers = "Y"; //Resources
+            chartSample.ChartAreas[0].AxisX.Minimum = double.NaN;
+            chartSample.ChartAreas[0].AxisX.IsMarginVisible = false;
+            chartSample.ChartAreas[0].AxisY.Minimum = double.NaN;
+            chartSample.ChartAreas[0].AxisY.IsMarginVisible = true;
+        }
+
+        //Display a message box.
+        DialogResult m1 = MessageBox.Show("Immediately enter your number.(Copy & Paste).\nInstead of typing it out.\nOkay?",
+                                           "ATTENTION", MessageBoxButtons.OK,
+                                           MessageBoxIcon.Information,
+                                           MessageBoxDefaultButton.Button1);
+
+        double resvalue;
+        double cusvalue;
+        private void txtCus_TextChanged(object sender, EventArgs e)
+        {
+            //if(!timer1.Enabled)
+            //{
+                if (double.TryParse(txtCus.Text, out cusvalue))
+                {
+                    resvalue = cusvalue / 2;
+                    txtRes.Text = resvalue.ToString();
+                    chartSample.Series["Customers"].Points.AddXY(0, cusvalue);
+                    chartSample.Series["Resources"].Points.AddXY(0, resvalue);
+
+                    //timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Error: Invalid Value.");
+                    //foreach (var series in chartSample.Series)
+                    //{
+                    //    series.Points.Clear();
+                    //}
+                }
+            //}
+        }
+
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    chartSample.Series[0].Points.AddXY(0, resvalue);
+        //    chartSample.Series[1].Points.AddXY(0, cusvalue);
+        //    if (cusvalue == (resvalue/2)) timer1.Stop();
+        //}
+
+        private void button1_Click(object sender, EventArgs e) //Reset Button
+        {
+            foreach (var series in chartSample.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Quit Button
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+    }
+}
