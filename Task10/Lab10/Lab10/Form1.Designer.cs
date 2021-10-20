f9c046f2ef2e3a02336b3ba21ed8ee0853b04931
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:02:23 +0700

    Add files via upload

---
+++ Task10/Lab10/Lab10/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(149, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 101);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(446, 112);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(106, 101);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(540, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Rolling Time:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(645, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Task10/Lab10/Lab10/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Array of different probabilities of 6 possible answers of Dice
+        double[] p = { 0.1, 0.25, 0.16, 0.14, 0.2, 0.15 };
+
+        int k;
+
+        Random random = new Random();
+        public double RandomDoubleInclusive() //We are using this method because random.NextDouble() method gives random number 
+                                              //between 0 and 1 where 0 is inclusive and 1 is exclusive. 
+                                              //Since the value of probability lies between 0 and 1, both inclusive that's why we need 
+                                              //to use this method.
+        {
+            double d = 0.0;
+            int i = 0;
+
+            do
+            {
+                d = random.NextDouble();
+                i = random.Next(2);
+            }
+            while (i == 1 && d > 0);
+
+
+
+            return d + i;
+
+
+
+        }
+      
+        public void functionfordice2() //Its a cheating dice
+        {
+            double A = RandomDoubleInclusive();
+            for (k = 1; k < 10;)
+            {
+
+                for (int i = 0; i < p.Length; i++) //Looping over array of probabilities
+                {
+                    A = A - p[i];
+
+
+
+
+                    if (A <= 0)
+                    {
+                        switch (k) // 9 Different cases in Cheating dice 
+                        {
+
+                            case 1:
+
+                                pictureBox2.Image = Properties.Resources.Dice1;
+
+                                break;
+                            case 2:
+                                pictureBox2.Image = Properties.Resources.Dice2;
+                                break;
+                            case 3:
+
+                                pictureBox2.Image = Properties.Resources.Dice3;
+
+                                break;
+                            case 4:
+
+                                pictureBox2.Image = Properties.Resources._4;
+                                break;
+                            case 5:
+
+                                pictureBox2.Image = Properties.Resources.Dice5;
+                                break;
+                            case 6:
+
+                                pictureBox2.Image = Properties.Resources.Dice6;
+                                break;
+
+                            case 7:
+
+                                pictureBox2.Image = Properties.Resources.Dice6;
+                                break;
+                            case 8:
+
+                                pictureBox2.Image = Properties.Resources.Dice6;
+                                break;
+                            case 9:
+
+                                pictureBox2.Image = Properties.Resources.Dice6;
+                                break;
+                        }
+
+                        break;
+
+
+                    }
+
+
+                    else
+                    {
+                        k++;
+
+                    }
+
+                }
+                break;
+
+
+            }
+
+
+
+
+        }
+
+        int rollingtime = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rollingtime++;
+
+            textBox1.Text = rollingtime.ToString();
+
+            int newroll = 0;
+            if (textBox1.Text == "20")
+            {
+                timer1.Stop();
+                rollingtime = newroll;
+            }
+
+            
+            functionfordice2();
+
+            double A = RandomDoubleInclusive();
+            for (k = 1; k < 7;)
+            {
+
+                for (int i = 0; i < p.Length; i++) //Looping over array of probabilities
+                {
+                    A = A - p[i];
+
+
+
+
+                    if (A <= 0)
+                    {
+                        switch (k) // 6 Different cases in Dices
+                        {
+
+                            case 1:
+
+                                pictureBox1.Image = Properties.Resources.Dice1;
+
+                                break;
+                            case 2:
+                                pictureBox1.Image = Properties.Resources.Dice2;
+                                break;
+                            case 3:
+
+                                pictureBox1.Image = Properties.Resources.Dice3;
+
+                                break;
+                            case 4:
+
+                                pictureBox1.Image = Properties.Resources._4;
+                                break;
+                            case 5:
+
+                                pictureBox1.Image = Properties.Resources.Dice5;
+                                break;
+                            case 6:
+
+                                pictureBox1.Image = Properties.Resources.Dice6;
+                                break;
+
+                        }
+
+                        break;
+
+
+                    }
+
+
+                    else
+                    {
+                        k++;
+
+                    }
+
+                }
+                break;
+
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
