6a10438378a0454fcc46ee946a81b2486583e3bb
sneha0909 <67375976+sneha0909@users.noreply.github.com>
2021-10-20 20:14:54 +0700

    Add files via upload

---
+++ Task13/Lab13/Lab13/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿
+namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Param1:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Param2:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number of Experiments:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(326, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Theoretical Probabilities";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(345, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(345, 84);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(103, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(345, 121);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(103, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(345, 162);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(103, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(345, 200);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(103, 22);
+            this.textBox8.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(524, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Emperical Probabilities";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(546, 43);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(103, 22);
+            this.textBox9.TabIndex = 14;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(546, 84);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(103, 22);
+            this.textBox10.TabIndex = 15;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(546, 121);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(103, 22);
+            this.textBox11.TabIndex = 16;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(546, 162);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(103, 22);
+            this.textBox12.TabIndex = 17;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(546, 200);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(103, 22);
+            this.textBox13.TabIndex = 18;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(1071, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Emperical Probabilities";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(578, 339);
+            this.chart1.TabIndex = 19;
+            this.chart1.Text = "chartSample";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(957, 191);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(99, 17);
+            this.label13.TabIndex = 60;
+            this.label13.Text = "> 11.07 is true";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(860, 188);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(91, 22);
+            this.textBox16.TabIndex = 59;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(691, 188);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(163, 17);
+            this.label12.TabIndex = 58;
+            this.label12.Text = "Chi-Squared Test Value:";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(925, 110);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(91, 22);
+            this.textBox15.TabIndex = 57;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(868, 113);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 17);
+            this.label11.TabIndex = 56;
+            this.label11.Text = "Error:";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(925, 50);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(91, 22);
+            this.textBox14.TabIndex = 55;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(868, 53);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 17);
+            this.label10.TabIndex = 54;
+            this.label10.Text = "Error:";
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(762, 113);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(91, 22);
+            this.textBox17.TabIndex = 53;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(691, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 52;
+            this.label9.Text = "Variance:";
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(762, 50);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(91, 22);
+            this.textBox18.TabIndex = 51;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(691, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 17);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Average:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1772, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Task13/Lab13/Lab13/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        double parameter;
+        int NumberofExperiments;
+        int m;
+        double S = 0;
+        double[] probabilities = new double[5]; //initializing an empty array
+
+        int[] Statistics = new int[5];
+        double[] Frequency = new double[5]; //initializing an empty array
+        int k;
+        Random random = new Random();
+
+        //int k;
+        double Mean;
+        double Variance;
+        double EmpiricExpectation;
+        double EmpiricVariance;
+        double AbsoluteErrorinMean;
+        double AbsoluteErrorinVariance;
+        double RelativeErrorinMean;
+        double RelativeErrorinVariance;
+        double ChiSquaredTestValue;
+
+
+
+        double p1;
+        double p2;
+        double p3;
+        double p4;
+        double p5;
+        public double RandomDoubleInclusive() //We are using this method because random.NextDouble() method gives random number 
+                                              //between 0 and 1 where 0 is inclusive and 1 is exclusive. 
+                                              //Since the value of probability lies between 0 and 1, both inclusive that's why we need 
+                                              //to use this method.
+        {
+            double d = 0.0;
+            int i = 0;
+
+            do
+            {
+                d = random.NextDouble();
+                i = random.Next(2);
+            }
+            while (i == 1 && d > 0);
+
+            return d + i;
+
+        }
+
+        public double factorial_Recursion(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            parameter = Convert.ToDouble(textBox1.Text);
+
+            //for (int z = 0; z < 5;)
+            //{
+
+                for (m = 0; m < 5;)
+                {
+
+                    double Alpha = RandomDoubleInclusive();
+
+                    double LogAlpha = Math.Log(Alpha);
+
+                    S = S + LogAlpha;
+
+                    double newLambda = parameter * -1;
+
+                    if (S < newLambda)
+                    {
+
+                        double subtractionfrom1   = 1 - parameter;                      //Using Geometric distribution which requires one parameter
+                        double powerofsubtraction = Math.Pow(subtractionfrom1, m);      // and calculating probabilities using the formula
+                                                                                        // P(x=m) = p(1 - p)^m
+                        probabilities[m] = parameter * powerofsubtraction;
+
+
+                        //double numerator = Math.Pow(Lambda, m);
+                        //double denominator = factorial_Recursion(m);
+                        //double MultiplierNum = Math.Exp(-Lambda);
+                        //probabilities[m] = (double)(numerator / denominator) * MultiplierNum;
+                        //z++;
+
+
+
+                        m++;
+                    }
+
+
+
+                    else
+                    {
+                        m = m + 1;
+
+                    }
+
+
+                }
+
+            //}
+
+            textBox4.Text = probabilities[0].ToString();         
+            textBox5.Text = probabilities[1].ToString();      
+            textBox6.Text = probabilities[2].ToString();          
+            textBox7.Text = probabilities[3].ToString();           
+            textBox8.Text = probabilities[4].ToString();
+           
+
+
+            int i;
+            NumberofExperiments = Convert.ToInt32(textBox3.Text);
+
+            for (i = 0; i <= NumberofExperiments;)
+            {
+
+                if (i < NumberofExperiments)
+                {
+                    k = random.Next(0, 5);
+
+                    Statistics[k] = Statistics[k] + 1;
+                    i++;
+
+                }
+
+                else
+                {
+
+                    for (int j = 0; j < 5; j++)
+                    {
+
+                        Frequency[j] = (double)Statistics[j] / NumberofExperiments;
+                        chart1.Series["Emperical Probabilities"].Points.AddXY(0, Frequency[j]);
+
+
+
+                    }
+                    break;
+
+
+
+                }
+
+            }
+
+            textBox9.Text = Frequency[0].ToString();
+            textBox10.Text = Frequency[1].ToString();
+            textBox11.Text = Frequency[2].ToString();
+            textBox12.Text = Frequency[3].ToString();
+            textBox13.Text = Frequency[4].ToString();
+
+            double P1 = Convert.ToDouble(textBox4.Text);
+            double P2 = Convert.ToDouble(textBox5.Text);
+            double P3 = Convert.ToDouble(textBox6.Text);
+            double P4 = Convert.ToDouble(textBox7.Text);
+            double P5 = Convert.ToDouble(textBox8.Text);
+
+            Mean = (P1 * 1) + (P2 * 2) + (P3 * 3) + (P4 * 4) + (P5 * 5);
+
+
+            double squareof1 = 1;
+            double squareof2 = 4;
+            double squareof3 = 9;
+            double squareof4 = 16;
+            double squareof5 = 25;
+
+            double squareofMean = Mean * Mean;
+
+            Variance = ((P1 * squareof1) + (P2 * squareof2) + (P3 * squareof3) + (P4 * squareof4) + (P5 * squareof5)) - squareofMean;
+
+            p1 = Frequency[0];
+            p2 = Frequency[1];
+            p3 = Frequency[2];
+            p4 = Frequency[3];
+            p5 = Frequency[4];
+
+
+            EmpiricExpectation = (p1 * 1) + (p2 * 2) + (p3 * 3) + (p4 * 4) + (p5 * 5);
+
+            textBox18.Text = EmpiricExpectation.ToString();
+
+            double squareofEmpiricExpectation = EmpiricExpectation * EmpiricExpectation;
+
+            EmpiricVariance = ((p1 * squareof1) + (p2 * squareof2) + (p3 * squareof3) + (p4 * squareof4) + (p5 * squareof5)) - squareofEmpiricExpectation;
+
+            textBox17.Text = EmpiricVariance.ToString();
+
+            AbsoluteErrorinMean = EmpiricExpectation - Mean;
+
+            AbsoluteErrorinVariance = EmpiricVariance - Variance;
+
+            RelativeErrorinMean = AbsoluteErrorinMean / Mean;
+
+            textBox14.Text = Math.Abs(RelativeErrorinMean).ToString();
+
+            RelativeErrorinVariance = AbsoluteErrorinVariance / Variance;
+
+            textBox15.Text = Math.Abs(RelativeErrorinVariance).ToString();
+
+
+            int multiplier = 1000;
+
+            int newp1 = (int)((p1 - (int)p1) * multiplier); //extracting last 3 digits of relative frequency
+            int newp2 = (int)((p2 - (int)p2) * multiplier);
+            int newp3 = (int)((p3 - (int)p3) * multiplier);
+            int newp4 = (int)((p4 - (int)p4) * multiplier);
+            int newp5 = (int)((p5 - (int)p5) * multiplier);
+
+            int Numerator1 = newp1 * newp1;
+            int Numerator2 = newp2 * newp2;
+            int Numerator3 = newp3 * newp3;
+            int Numerator4 = newp4 * newp4;
+            int Numerator5 = newp5 * newp5;
+
+
+            double denominator1 = NumberofExperiments * P1;
+            double denominator2 = NumberofExperiments * P2;
+            double denominator3 = NumberofExperiments * P3;
+            double denominator4 = NumberofExperiments * P4;
+            double denominator5 = NumberofExperiments * P5;
+
+            double ChiValue1 = Math.Abs((Numerator1 / denominator1) - NumberofExperiments);
+            double ChiValue2 = Math.Abs((Numerator2 / denominator2) - NumberofExperiments);
+            double ChiValue3 = Math.Abs((Numerator3 / denominator3) - NumberofExperiments);
+            double ChiValue4 = Math.Abs((Numerator4 / denominator4) - NumberofExperiments);
+            double ChiValue5 = Math.Abs((Numerator5 / denominator5) - NumberofExperiments);
+
+            ChiSquaredTestValue = ChiValue1 + ChiValue2 + ChiValue3 + ChiValue4 + ChiValue5;
+            textBox16.Text = ChiSquaredTestValue.ToString();
+
+
+
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
